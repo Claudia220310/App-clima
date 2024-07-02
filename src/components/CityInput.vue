4a8f2039caab367bc3bc5edcda6726e9beeba628
Claudia220310 <norena1466@gmail.com>
2024-07-01 20:19:06 -0500

    Estructura para el ingreso del nombre de la ciudad.

---
+++ src/components/CityInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" v-model="cityName" placeholder="Ingrese el nombre de la ciudad" />
+    <button @click="searchCity">Buscar</button>
+  </div>
+</template>
+
+<script lang="ts">
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'CityInput',
+  data() {
+    return {
+      cityName: '',
+    };
+  },
+  methods: {
+    searchCity() {
+      this.$emit('city-selected', this.cityName);
+    },
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
