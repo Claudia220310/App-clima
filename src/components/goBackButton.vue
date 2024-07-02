c0cb092c4de1d3abf95f93f733dade855c9e9634
Claudia220310 <norena1466@gmail.com>
2024-07-02 17:58:22 -0500

    Se creó componente para volver a home.

---
+++ src/components/goBackButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <ion-button @click="goBack">Ir a inicio</ion-button>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import { IonButton } from '@ionic/vue';
+  
+  export default defineComponent({
+    name: 'GoBackButton',
+    components: {
+      IonButton,
+    },
+    methods: {
+      goBack() {
+        this.$router.push('/home');
+      },
+    },
+  });
+  </script>
+  
+  <style scoped>
+  </style>
+  
