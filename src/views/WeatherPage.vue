457d2214742147ff6607fdf2a1b8f59e8e2cd9d6
Claudia220310 <norena1466@gmail.com>
2024-07-02 17:07:08 -0500

    Se creó la vista donde se ven los detalles del clima.

---
+++ src/views/WeatherPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Detalles del clima</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <weather-details :city="city" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import WeatherDetails from '../components/WeatherDetails.vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+
+export default defineComponent({
+  name: 'WeatherPage',
+  components: {
+    WeatherDetails,
+    IonPage,
+    IonToolbar,
+    IonHeader,
+    IonTitle,
+    IonContent,
+  },
+  data() {
+    return {
+      city: this.$route.query.city || '',
+    };
+  },
+});
+</script>
+
+<style scoped>
+</style>
