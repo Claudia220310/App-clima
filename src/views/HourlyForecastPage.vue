c5ea3e9c2fdf9ecc8c9578dfc5878bb4d6d76194
Claudia220310 <norena1466@gmail.com>
2024-07-02 18:01:53 -0500

    Se creó vista para obtener el pronóstico por hora.

---
+++ src/views/HourlyForecastPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <ion-page>
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Pronóstico por Hora</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <hourly-forecast :city="city" />
+      </ion-content>
+    </ion-page>
+</template>
+  
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import HourlyForecast from '../components/HourlyForecast.vue';
+  import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+  
+  export default defineComponent({
+    name: 'HourlyForecastPage',
+    components: {
+      HourlyForecast,
+      IonPage,
+      IonToolbar,
+      IonHeader,
+      IonTitle,
+      IonContent,
+    },
+    data() {
+      return {
+        city: this.$route.query.city || '',
+      };
+    },
+  });
+</script>
+  
+<style scoped>
+</style>
+  
