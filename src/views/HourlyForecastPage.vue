41fd56304c8c7bcf3072cd6293796209768c3757
Claudia220310 <norena1466@gmail.com>
2024-07-04 01:27:34 -0500

    Se modificó para que reinicie la información a la consulta actual

--- src/views/HourlyForecastPage.vue
+++ src/views/HourlyForecastPage.vue
@@ -1,39 +1,52 @@
 <template>
-    <ion-page>
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Pronóstico por Hora</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content>
-        <hourly-forecast :city="city" />
-      </ion-content>
-    </ion-page>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Pronóstico por Hora</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <hourly-forecast :city="city" />
+      <go-back-button />
+    </ion-content>
+  </ion-page>
 </template>
-  
+
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import HourlyForecast from '../components/HourlyForecast.vue';
-  import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-  
-  export default defineComponent({
-    name: 'HourlyForecastPage',
-    components: {
-      HourlyForecast,
-      IonPage,
-      IonToolbar,
-      IonHeader,
-      IonTitle,
-      IonContent,
-    },
-    data() {
-      return {
-        city: this.$route.query.city || '',
-      };
-    },
-  });
+import { defineComponent, watch } from 'vue';
+import HourlyForecast from '../components/HourlyForecast.vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { useRoute } from 'vue-router';
+import GoBackButton from '../components/goBackButton.vue';
+
+export default defineComponent({
+  name: 'HourlyForecastPage',
+  components: {
+    HourlyForecast,
+    IonPage,
+    IonToolbar,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    GoBackButton,
+  },
+  data() {
+    return {
+      city: '',
+    };
+  },
+  mounted() {
+    const route = useRoute();
+    watch(
+      () => route.query.city,
+      (newCity) => {
+        this.city = newCity;
+      },
+      { immediate: true }
+    );
+  },
+});
 </script>
-  
+
 <style scoped>
 </style>
-  
